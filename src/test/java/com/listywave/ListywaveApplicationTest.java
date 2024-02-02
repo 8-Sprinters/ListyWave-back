96a7444b033a04dc5c3acafad630700f823ad946
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-02-02 13:03:41 +0900

    chore: dev 서버 ddl-auto 옵션 수정 및 더미 데이터를 위한 data.sql 작성 (#44)
    * chore: 더미 데이터를 위한 sql 파일 작성 (#39)
    
    * chore: 빈 컨테이너 load 확인을 위한 테스트 복구 (#39)
    
    * chore: 로컬 환경에서 정상 동작을 위한 application.yml 파일 수정 (#39)
    
    * feat: User 엔티티에 allPrivate 필드 추가 (#39)

---
+++ src/test/java/com/listywave/ListywaveApplicationTest.java
@@ -0,0 +1,12 @@
+package com.listywave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListywaveApplicationTest {
+
+    @Test
+    void contextLoadTest() {
+    }
+}
