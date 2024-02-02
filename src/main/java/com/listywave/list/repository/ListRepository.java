528d2fb0d80f61281d7c5e7a4eee24409a5dc209
kdkdhoho <hkim4410@naver.com>
2024-02-02 22:13:57 +0900

    feat: 리스트 상세 조회 기능 구현 (#42)

--- src/main/java/com/listywave/list/repository/ListRepository.java
+++ src/main/java/com/listywave/list/repository/ListRepository.java
@@ -1,7 +1,14 @@
 package com.listywave.list.repository;
 
+import static com.listywave.common.exception.ErrorCode.NOT_FOUND;
+
+import com.listywave.common.exception.CustomException;
 import com.listywave.list.application.domain.Lists;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ListRepository extends JpaRepository<Lists, Long> {
+
+    default Lists getById(Long listId) {
+        return findById(listId).orElseThrow(() -> new CustomException(NOT_FOUND));
+    }
 }
