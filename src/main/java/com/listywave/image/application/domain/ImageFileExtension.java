09f9327135eaceb4760a1a6329c8aed3a424d22b
coPpark <qkrwjdtn963@gmail.com>
2024-02-03 22:55:45 +0900

    Hotfix/extension (#55)
    * hotfix: 리스트부분 nullable 가능 부분 수정 (#dev)
    
    * hotfix: 이미지 확장자 소문자 요청 가능하도록 수정 (#dev)

--- src/main/java/com/listywave/image/application/domain/ImageFileExtension.java
+++ src/main/java/com/listywave/image/application/domain/ImageFileExtension.java
@@ -1,5 +1,9 @@
 package com.listywave.image.application.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.listywave.common.exception.CustomException;
+import com.listywave.common.exception.ErrorCode;
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,4 +17,12 @@ public enum ImageFileExtension {
     ;
 
     private final String uploadExtension;
+
+    @JsonCreator
+    public static ImageFileExtension fromString(String key) {
+        return Arrays.stream(ImageFileExtension.values())
+                .filter(extensionType -> extensionType.name().equalsIgnoreCase(key))
+                .findFirst()
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "해당 이미지 확장자가 존재하지 않습니다."));
+    }
 }
