9bf42803878e87d08b8d0048e47eb0c89dc3e6e3
kdkdhoho <hkim4410@naver.com>
2024-02-03 15:51:24 +0900

    feat: 답글 삭제 API 구현 (#43)

--- src/main/java/com/listywave/list/application/service/ReplyService.java
+++ src/main/java/com/listywave/list/application/service/ReplyService.java
@@ -3,27 +3,32 @@ package com.listywave.list.application.service;
 import com.listywave.auth.application.domain.JwtManager;
 import com.listywave.list.application.domain.Comment;
 import com.listywave.list.application.domain.Reply;
+import com.listywave.list.application.dto.ReplyDeleteCommand;
 import com.listywave.list.application.dto.response.ReplyCreateResponse;
 import com.listywave.list.application.vo.Content;
 import com.listywave.list.repository.CommentRepository;
+import com.listywave.list.repository.ListRepository;
 import com.listywave.list.repository.ReplyRepository;
 import com.listywave.user.application.domain.User;
 import com.listywave.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ReplyService {
 
     private final JwtManager jwtManager;
+    private final ListRepository listRepository;
     private final UserRepository userRepository;
     private final ReplyRepository replyRepository;
     private final CommentRepository commentRepository;
 
-    public ReplyCreateResponse createReply(Long commentId, String accessToken, String content) {
+    public ReplyCreateResponse createReply(Long listId, Long commentId, String accessToken, String content) {
+        listRepository.getById(listId);
         Long writerId = jwtManager.read(accessToken);
-
         User user = userRepository.getById(writerId);
         Comment comment = commentRepository.getReferenceById(commentId);
 
@@ -31,5 +36,14 @@ public class ReplyService {
         Reply saved = replyRepository.save(reply);
 
         return ReplyCreateResponse.of(saved, comment, user);
+    }
+
+    public void delete(ReplyDeleteCommand command) {
+        Long writerId = jwtManager.read(command.accessToken());
+        userRepository.getById(writerId);
+        listRepository.getById(command.listId());
+        commentRepository.getById(command.commentId());
+
+        replyRepository.deleteById(command.replyId());
     }
 }
--- src/main/java/com/listywave/list/presentation/controller/ReplyController.java
+++ src/main/java/com/listywave/list/presentation/controller/ReplyController.java
@@ -2,11 +2,13 @@ package com.listywave.list.presentation.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
+import com.listywave.list.application.dto.ReplyDeleteCommand;
 import com.listywave.list.application.dto.response.ReplyCreateResponse;
 import com.listywave.list.application.service.ReplyService;
 import com.listywave.list.presentation.dto.request.ReplyCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,12 +25,24 @@ public class ReplyController {
 
     @PostMapping
     ResponseEntity<ReplyCreateResponse> create(
+            @PathVariable(value = "listId") Long listId,
             @PathVariable(value = "commentId") Long commentId,
             @RequestHeader(value = "Authorization", defaultValue = "") String accessToken,
             @RequestBody ReplyCreateRequest request
     ) {
-        ReplyCreateResponse response = replyService.createReply(commentId, accessToken, request.content());
+        ReplyCreateResponse response = replyService.createReply(listId, commentId, accessToken, request.content());
         return ResponseEntity.status(CREATED).body(response);
     }
 
+    @DeleteMapping("/{replyId}")
+    ResponseEntity<Void> deleteReply(
+            @PathVariable(value = "listId") Long listId,
+            @PathVariable(value = "commentId") Long commentId,
+            @PathVariable(value = "replyId") Long replyId,
+            @RequestHeader(value = "Authorization", defaultValue = "") String accessToken
+    ) {
+        ReplyDeleteCommand replyDeleteCommand = new ReplyDeleteCommand(listId, commentId, replyId, accessToken);
+        replyService.delete(replyDeleteCommand);
+        return ResponseEntity.noContent().build();
+    }
 }
