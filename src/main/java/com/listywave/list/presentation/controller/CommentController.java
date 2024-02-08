8b2ea288b0dd89a7f50ed913f407f75cb277a6cf
kdkdhoho <hkim4410@naver.com>
2024-02-08 11:08:09 +0900

    hotfix: 프론트에서 댓글 생성, 삭제 테스트를 위한 임시 코드 주석처리

--- src/main/java/com/listywave/list/presentation/controller/CommentController.java
+++ src/main/java/com/listywave/list/presentation/controller/CommentController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,10 +27,10 @@ public class CommentController {
     @PostMapping
     ResponseEntity<CommentCreateResponse> create(
             @PathVariable(value = "listId") Long listId,
-            @RequestHeader(value = "Authorization", defaultValue = "") String accessToken,
+//            @RequestHeader(value = "Authorization", defaultValue = "") String accessToken,
             @RequestBody CommentCreateRequest commentCreateRequest
     ) {
-        CommentCreateResponse response = commentService.create(listId, accessToken, commentCreateRequest.content());
+        CommentCreateResponse response = commentService.create(listId, commentCreateRequest.content());
         return ResponseEntity.status(CREATED).body(response);
     }
 
@@ -48,10 +47,10 @@ public class CommentController {
     @DeleteMapping("/{commentId}")
     ResponseEntity<Void> delete(
             @PathVariable(value = "listId") Long listId,
-            @RequestHeader(value = "Authorization", defaultValue = "") String accessToken,
+//            @RequestHeader(value = "Authorization", defaultValue = "") String accessToken,
             @PathVariable(value = "commentId") Long commentId
     ) {
-        commentService.delete(listId, accessToken, commentId);
+        commentService.delete(listId, commentId);
         return ResponseEntity.noContent().build();
     }
 }
