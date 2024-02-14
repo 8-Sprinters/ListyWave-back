09b6084d294b59498d3dabd6db7c3c2688fd796d
kdkdhoho <hkim4410@naver.com>
2024-02-14 09:09:37 +0900

    refactor: ErrorResponse, ExceptionHandler 리팩터링 (#113)

--- src/main/java/com/listywave/common/exception/ErrorResponse.java
+++ src/main/java/com/listywave/common/exception/ErrorResponse.java
@@ -3,26 +3,23 @@ package com.listywave.common.exception;
 import org.springframework.http.ResponseEntity;
 
 public record ErrorResponse(
-    int status,
-    String error,
-    String code,
-    String detail,
-    String message
+        int status,
+        String error,
+        String code,
+        String detail,
+        String message
 ) {
 
-    public static ResponseEntity<ErrorResponse> toResponseEntity(CustomException e){
+    public static ResponseEntity<ErrorResponse> toResponseEntity(CustomException e) {
         ErrorCode errorCode = e.getErrorCode();
-
-        return ResponseEntity
-                .status(errorCode.getStatus())
-                .body(
-                        new ErrorResponse(
-                                errorCode.getStatus().value(),
-                                errorCode.getStatus().name(),
-                                errorCode.name(),
-                                errorCode.getDetail(),
-                                e.getMessage()
-                        )
-                );
+        ErrorResponse errorResponse = new ErrorResponse(
+                errorCode.getStatus().value(),
+                errorCode.getStatus().name(),
+                errorCode.name(),
+                errorCode.getDetail(),
+                e.getMessage()
+        );
+        
+        return ResponseEntity.status(errorCode.getStatus()).body(errorResponse);
     }
 }
