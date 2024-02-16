e0a09403049bcc62eed3bd169027358c52370538
kdkdhoho <hkim4410@naver.com>
2024-02-16 18:12:36 +0900

    refactor: CategoryTypeConverter 예외 처리 (#123)

--- src/main/java/com/listywave/list/application/domain/category/CategoryTypeConverter.java
+++ src/main/java/com/listywave/list/application/domain/category/CategoryTypeConverter.java
@@ -5,17 +5,17 @@ import jakarta.persistence.AttributeConverter;
 public class CategoryTypeConverter implements AttributeConverter<CategoryType, String> {
 
     @Override
-    public String convertToDatabaseColumn(CategoryType attribute) {
-        if (attribute == null) {
-            return null;
+    public String convertToDatabaseColumn(CategoryType categoryType) {
+        if (categoryType == null) {
+            throw new NullPointerException("CategoryType을 DB 칼럼으로 변경하는 과정에서 null이 포함되었습니다.");
         }
-        return attribute.getCodeValue();
+        return categoryType.getCodeValue();
     }
 
     @Override
     public CategoryType convertToEntityAttribute(String dbData) {
         if (dbData == null) {
-            return null;
+            throw new NullPointerException("List 테이블의 category_id 값이 null입니다.");
         }
         return CategoryType.enumOf(dbData);
     }
