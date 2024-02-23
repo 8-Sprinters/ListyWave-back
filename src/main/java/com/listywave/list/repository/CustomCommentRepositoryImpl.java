43d1078fa9b8b94e7b81da4c56c8e9e2fb59eeb9
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-02-23 13:54:20 +0900

    fix: 댓글 조회 및 답글 생성 관련 버그 해결 (#188)
    * fix: reply 테이블과 inner join으로 인해 댓글이 조회되지 않는 문제 해결 (#186)
    
    * fix: 답글 생성 시 실패하는 문제 해결 (#186)

--- src/main/java/com/listywave/list/repository/CustomCommentRepositoryImpl.java
+++ src/main/java/com/listywave/list/repository/CustomCommentRepositoryImpl.java
@@ -3,10 +3,10 @@ package com.listywave.list.repository;
 
 import static com.listywave.list.application.domain.comment.QComment.comment;
 import static com.listywave.list.application.domain.list.QListEntity.listEntity;
-import static com.listywave.list.application.domain.reply.QReply.reply;
 
 import com.listywave.list.application.domain.comment.Comment;
 import com.listywave.list.application.domain.list.ListEntity;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -19,15 +19,21 @@ public class CustomCommentRepositoryImpl implements CustomCommentRepository {
     @Override
     public List<Comment> getComments(ListEntity list, int size, Long cursorId) {
         return queryFactory.selectFrom(comment)
-                .join(listEntity).fetchJoin().on(listEntity.id.eq(comment.list.id))
-                .join(reply).fetchJoin().on(comment.id.eq(reply.comment.id))
+                .leftJoin(listEntity).on(comment.list.id.eq(listEntity.id)).fetchJoin()
                 .where(
-                        listEntity.id.eq(list.getId()),
-                        comment.id.gt(cursorId),
+                        comment.list.id.eq(list.getId()),
+                        commentIdGt(cursorId),
                         comment.user.isDelete.eq(false)
                 )
                 .orderBy(comment.id.asc())
                 .limit(size + 1)
                 .fetch();
+    }
+
+    private static BooleanExpression commentIdGt(Long cursorId) {
+        if (cursorId == null) {
+            return null;
+        }
+        return comment.id.gt(cursorId);
     }
 }
