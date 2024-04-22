f3c63929e4fce7f7cb3b432b418b4bf915ab1a7d
coPpark <qkrwjdtn963@gmail.com>
2024-04-22 20:58:56 +0900

    feat: scheduler 이용하여 30일 지난 알림 삭제 주기적으로 삭제 기능 구현 (#250)
    * feat: scheduler 이용하여 30일 지난 알림 삭제 주기적으로 삭제 기능 구현 (#136)
    
    * fix: 메서드명 변경 (#136)

--- src/main/java/com/listywave/ListywaveApplication.java
+++ src/main/java/com/listywave/ListywaveApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = {"com.listywave"})
---
+++ src/main/java/com/listywave/alarm/application/service/AlarmSchedulerService.java
@@ -0,0 +1,26 @@
+package com.listywave.alarm.application.service;
+
+import com.listywave.alarm.repository.AlarmRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AlarmSchedulerService {
+
+    private final AlarmRepository alarmRepository;
+
+    @Async
+    @Transactional
+    @Scheduled(cron = "0 0 0 * * *")
+    public void deleteAlarmByScheduler() {
+        log.info("\n ============== alarmDeleteScheduler start ================ \n");
+        alarmRepository.deleteAlarmThirtyDaysAgo();
+        log.info("\n ============== alarmDeleteScheduler end ================ \n");
+    }
+}
