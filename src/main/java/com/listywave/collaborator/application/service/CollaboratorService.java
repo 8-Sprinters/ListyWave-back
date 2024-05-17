54cd80b73bff6f5f3b81d9bb0aa65a66420c7079
kdkdhoho <hkim4410@naver.com>
2024-05-17 17:15:28 +0900

    refactor: ListService가 CollaboratorService만을 의존하도록 리팩터링 (#258)

--- src/main/java/com/listywave/collaborator/application/service/CollaboratorService.java
+++ src/main/java/com/listywave/collaborator/application/service/CollaboratorService.java
@@ -1,7 +1,6 @@
 package com.listywave.collaborator.application.service;
 
 import static org.springframework.transaction.annotation.Propagation.REQUIRED;
-import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 
 import com.listywave.collaborator.application.domain.Collaborator;
 import com.listywave.collaborator.application.domain.Collaborators;
@@ -20,7 +19,7 @@ public class CollaboratorService {
     private final UserRepository userRepository;
     private final CollaboratorRepository collaboratorRepository;
 
-    @Transactional(readOnly = true, propagation = REQUIRES_NEW)
+    @Transactional(readOnly = true, propagation = REQUIRED)
     public Collaborators createCollaborators(List<Long> userIds, ListEntity list) {
         return new Collaborators(userIds.stream()
                 .map(userRepository::getById)
@@ -36,5 +35,24 @@ public class CollaboratorService {
     @Transactional(propagation = REQUIRED)
     public List<Collaborator> saveAll(Collaborators collaborators) {
         return collaboratorRepository.saveAll(collaborators.collaborators());
+    }
+
+    @Transactional(propagation = REQUIRED)
+    public void updateCollaborators(Collaborators beforeCollaborators, Collaborators newCollaborators) {
+        Collaborators removedCollaborators = beforeCollaborators.filterRemovedCollaborators(newCollaborators);
+        collaboratorRepository.deleteAllInBatch(removedCollaborators.collaborators());
+
+        Collaborators addedCollaborators = beforeCollaborators.filterAddedCollaborators(newCollaborators);
+        collaboratorRepository.saveAll(addedCollaborators.collaborators());
+    }
+
+    @Transactional(propagation = REQUIRED)
+    public void deleteAllByList(ListEntity list) {
+        collaboratorRepository.deleteAllByList(list);
+    }
+
+    @Transactional(propagation = REQUIRED)
+    public void deleteAllByListIn(List<ListEntity> lists) {
+        collaboratorRepository.deleteAllByListIn(lists);
     }
 }
