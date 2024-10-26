322bcee3825d19a6b3a4763b6e26386ac4cd28ae
kdkdhoho <hkim4410@naver.com>
2024-10-26 17:50:16 +0900

    test: 토픽 생성 테스트 검증 수정 (#306)

--- src/test/java/com/listywave/topic/application/service/TopicServiceTest.java
+++ src/test/java/com/listywave/topic/application/service/TopicServiceTest.java
@@ -32,20 +32,14 @@ class TopicServiceTest extends IntegrationTest {
 
             // then
             List<Topic> result = topicRepository.findAll();
-            Topic expect = Topic.builder()
-                    .user(ej)
-                    .title(new ListTitle("제일 좋아하는 여자 아이돌 TOP3"))
-                    .description(new ListDescription("여러분들은 어떤 여돌을 가장 좋아하나요?"))
-                    .category(DAILYLIFE_THOUGHTS)
-                    .build();
-
             assertAll(
                     () -> assertThat(result).hasSize(1),
                     () -> {
                         Topic topic = result.get(0);
-                        assertThat(topic).usingRecursiveComparison()
-                                .ignoringFields("id", "createdDate", "updatedDate")
-                                .isEqualTo(expect);
+                        assertThat(topic.getCategory()).isEqualTo(DAILYLIFE_THOUGHTS);
+                        assertThat(topic.getTitle().getValue()).isEqualTo("제일 좋아하는 여자 아이돌 TOP3");
+                        assertThat(topic.getDescription().getValue()).isEqualTo("여러분들은 어떤 여돌을 가장 좋아하나요?");
+                        assertThat(topic.isAnonymous()).isFalse();
                         assertThat(topic.isExposed()).isFalse();
                     }
             );
